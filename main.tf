7b054ede40af2ae25d520e41daea813419492293
offongbassey <offongbassey23@gmail.com>
2026-02-04 12:59:32 +0100

    main.tf update

--- main.tf
+++ main.tf
@@ -1,25 +1,24 @@
 terraform {
-    required_version = "> = 1.0"
+  required_version = ">= 1.0"
 
-    required_providers {
-        aws = {
-            source  = "hashicorp/aws"
-            version = "~> 5.0"
-        }
+  required_providers {
+    aws = {
+      source  = ""hashicorp/aws"
+      version = "~>5.0"
     }
+  }
 }
 
 provider "aws" {
-    region = var.aws_region
-
-    default_tags {
-        tags = {
-            Project     = "var.project_name"
-            Environment = "dev"
-            ManagedBy   = Terraform"
-
-        }
+  region = var.aws_region
+  
+  default_tags {
+    tags = {
+      Project     = var.project_name
+      Environment = "dev"
+      ManagedBy   = "Terraform"
     }
+  }
 }
 
 # Create S3 bucket that will trigger Lambda
